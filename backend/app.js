33e384337fc2c8d67b2cf6f93d0eecae4320a272
Pauline Bourgeais <bourgeais.pauline@gmail.com>
2021-12-08 18:11:19 +0100

    add helmet package

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,39 @@
-// imports Express (framework NodeJS) et Sequelize
+// imports
 const express = require('express');
 const { Sequelize } = require('sequelize');
-
-// import Routes
-const userRoutes = require('./routes/user');
-const postRoutes = require('./routes/post');
-
-
-// import méthodes path et express
 const path = require('path'); 
 const app = express();
+const helmet = require("helmet");
+
+
+// import routers
+const userRoutes = require('./routes/user');
+const postRoutes = require('./routes/post');
+const commentRoutes = require('./routes/comment');
+
+
+// middleware pour contourner les erreurs de CORS
+app.use((req, res, next) => {
+  // permet d'accéder à l'API depuis n'importe quelle origine
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // permet d'ajouter les headers mentionnés aux requêtes envoyées vers l'API
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  // permet d'envoyer des requêtes avec les méthodes mentionnées (GET, ...)
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  // appeler next pour passer au middleware suivant
+  next(); 
+});
 
 
 // transformer le corps de la requête en JSON
 app.use(express.json());
+// middleware pour sécuriser l'app Express en définissant divers headers
+app.use(helmet());
+// middleware pour dire à l'application de se servir du dossier images
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
-// charge les variables d'environnement pour se connecter à la BDD
+
+// variables d'environnement pour se connecter à la BDD
 require('dotenv').config();
 
 const db = new Sequelize(process.env.DB_HOST, process.env.DB_USER, process.env.DB_PASS, {
@@ -26,7 +44,7 @@ const db = new Sequelize(process.env.DB_HOST, process.env.DB_USER, process.env.D
 // test de connection
 const testConnection = async () => {
   try {
-      await db.authenticate();
+      await sequelize.authenticate();
       console.log('Connection has been established successfully.');
   } catch (error) {
       console.error('Unable to connect to the database:', error);
@@ -36,6 +54,12 @@ testConnection();
 
 
 
+
+
+// routes
+app.use('/api/user', userRoutes);
+app.use("/api/post", postRoutes);
+app.use("/api/comment", commentRoutes);
 
 
 
