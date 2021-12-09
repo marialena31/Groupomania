c964396a5ef214fa82b3e573a73f4942fdf14b1c
Pauline Bourgeais <bourgeais.pauline@gmail.com>
2021-12-09 18:19:12 +0100

    update user model

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,6 +1,47 @@
 /* MODEL DE DONNEES UTILISATEUR */
-'use strict';
-const { Model } = require('sequelize');
+const sequelize = require('sequelize');
+const db = require('../db');
+
+const user = db.define('User', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    firstname: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lastname: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    avatarUrl: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    bio: {
+        type: DataTypes.TEXT
+    },
+    isAdmin: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+    }
+});
+module.exports = user;
+
+
+
+/*const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
     class User extends Model {
@@ -52,4 +93,7 @@ module.exports = (sequelize, DataTypes) => {
         }
     );
     return User;
-  };
+  };*/
+
+
+  db.sync()
